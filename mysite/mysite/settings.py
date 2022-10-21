ed9bdb0352a1e45ed4d10093740026f50d2d441b
Austin Prince <amorrisprince@gmail.com>
2022-10-20 20:36:02 -0700

    commiting for Azure web apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)!7u0^s8dx!lb=$e%v(5neq1b^zqpwp$hmg^_6cgw$wxx_ahg7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
